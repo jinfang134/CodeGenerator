4eef9770f1113f9e24ba76fe10aaffeada8a6de8
jinfang <jinfang134@sina.com>
2017-04-05 20:32:37 +0800

    remove useless import.

--- src/main/java/com/gdnyt/dao/MysqlTableDao.java
+++ src/main/java/com/gdnyt/dao/MysqlTableDao.java
@@ -1,23 +1,15 @@
 package com.gdnyt.dao;
 
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.gdnyt.model.Column;
 import com.gdnyt.model.Table;
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
 
 @Repository
 public class MysqlTableDao implements TableDao{
