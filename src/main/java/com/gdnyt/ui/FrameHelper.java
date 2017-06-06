59655419c9aa1395b949a784da996bfd44b2fae0
jinfang <jinfang134@sina.com>
2017-06-06 08:51:19 +0800

    添加了markdown样式

--- src/main/java/com/gdnyt/ui/FrameHelper.java
+++ src/main/java/com/gdnyt/ui/FrameHelper.java
@@ -1,29 +1,58 @@
 package com.gdnyt.ui;
 
 import java.awt.ScrollPane;
-import java.awt.Scrollbar;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.List;
 
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 
 import jodd.io.FileUtil;
 
-import org.commonmark.Extension;
-import org.commonmark.ext.gfm.tables.TablesExtension;
-import org.commonmark.node.Node;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.profiles.pegdown.Extensions;
+import com.vladsch.flexmark.profiles.pegdown.PegdownOptionsAdapter;
+import com.vladsch.flexmark.util.options.DataHolder;
+import com.vladsch.flexmark.util.options.MutableDataSet;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.ScrollPaneConstants;
 
 public class FrameHelper extends JFrame {
+	static final DataHolder OPTIONS = PegdownOptionsAdapter
+			.flexmarkOptions(Extensions.ALL);
 
 	public FrameHelper() {
 		init();
 	}
+
+	// private String markdown() {
+	// URL url = FrameMain.class.getResource("/markdown/string.md");
+	// File file = new File(url.getFile());
+	// String md = null;
+	// try {
+	// md = FileUtil.readString(file);
+	// } catch (IOException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// // List<Extension> extensions = Arrays.asList(TablesExtension.create());
+	// // Parser parser = Parser.builder().extensions(extensions).build();
+	// // Node document = parser.parse(md);
+	// // HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions)
+	// // .build();
+	// //
+	// // String html = renderer.render(document); //
+	// "<p>This is <em>Sparta</em></p>\n"
+	// // return html;
+	// }
 
 	private String markdown() {
 		URL url = FrameMain.class.getResource("/markdown/string.md");
@@ -35,34 +64,47 @@ public class FrameHelper extends JFrame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		MutableDataSet options = new MutableDataSet();
+		Parser parser = Parser.builder(OPTIONS).build();
+		HtmlRenderer renderer = HtmlRenderer.builder(OPTIONS).build();
 
-		List<Extension> extensions = Arrays.asList(TablesExtension.create());
-		Parser parser = Parser.builder().extensions(extensions).build();
+		// You can re-use parser and renderer instances
 		Node document = parser.parse(md);
-		HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions)
-				.build();
-
 		String html = renderer.render(document); // "<p>This is <em>Sparta</em></p>\n"
+		System.out.println(html);
 		return html;
 	}
 
 	private void init() {
 		URL url = FrameMain.class.getResource("/html/test.html");
 		File file = new File(url.getFile());
-
+		String data="<html><head><link rel=\"stylesheet\" href=\"./markdown.css\"/></head>"
+				+ "<body><div class=\"markdown-body\">"+markdown()+"</div></body></html>";
+		try {
+			FileUtil.writeString(file, data);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		JEditorPane editorPane = new JEditorPane();
-		String pathString = "http://" + file.getPath();
+		String pathString = "file://" + file.getPath();
 		editorPane.setEditable(false);
 		editorPane.setContentType("text/html");
-		editorPane
-				.setText("<html><head><style> table {width: 100%;border-collapse: collapse;border: 1px solid #ccc;} "
-						+ "thead td {font-size: 12px;color: #333333;text-align: center;border: 1px solid #ccc; font-weight:bold;}"
-						+ " tbody tr {background: #fff;font-size: 12px;color: #666666;}  "
-						+ " td{line-height:20px;text-align: left;padding:4px 10px 3px 10px;height: 18px;border: 1px solid #ccc;} "
-						+ "</style></head>" + markdown() + "</html>");// (pathString);
-		ScrollPane scrollPane = new ScrollPane();
-		scrollPane.add(editorPane);
-		add(scrollPane);
+		try {
+			editorPane.setPage(pathString);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	    //  scrollPane.setBounds(14, 39, 616, 374);
+		scrollPane.setViewportView(editorPane);
+//		getContentPane().add(scrollPane);
+		
+		
+		getContentPane().add(scrollPane);
 		setSize(500, 300);
 		setVisible(true);
 	}
--- src/main/resources/markdown/string.md
+++ src/main/resources/markdown/string.md
@@ -26,3 +26,50 @@ Markdown 是一种方便记忆、书写的纯文本标记语言，用户可以�
 - [x] 新增 Todo 列表功能
 - [x] 修复 LaTex 公式渲染问题
 - [x] 新增 LaTex 公式编号功能
+
+```
+private static void main(){
+	String a="";
+	
+	}
+```
+
+`sdfsdfdsf`
+
+> this is quote
+
+
+
+## 字段说明
+
+### 数据表（table）
+
+描述数据库中的某一个表 主要包括以下变量：
+
+| 字段名         | 类型           | 描述                    |
+| ----------- | ------------ | --------------------- |
+| packagename | String       | 包名，即基础包名              |
+| name        | String       | 数据库中的原始表名             |
+| columnlist  | List<Column> | 列集合                   |
+| comment     | String       | 表备注                   |
+| classname   | String       | 类名（即去掉表名前缀，并将首字母转为大写） |
+| lowername   | String       | 去掉前缀的表名               |
+
+### 列（column）
+
+描述数据表中的某一列,主要包括以下变量
+
+| 名称         | 类型     | 说明                                |
+| ---------- | ------ | --------------------------------- |
+| name       | String | 列名（去掉了_，并转为驼峰命名法）                 |
+| comment    | String | 备注，建议默认在数据库里都写上备注，代码里的所有注释都来源于此备注 |
+| type       | String | Java数据类型                          |
+| length     | int    | 数据长度                              |
+| bigname    | String | 将列名变为首字母大写                        |
+| columnname | String | 数据表中的原始列名                         |
+| isnull     | String | 是否为空（YES或者NO）                     |
+| columntype | String | 数据库中的数据类型                         |
+| columnkey  | String | 是否主键                              |
+| extra      | String | 其它                                |
+
+
